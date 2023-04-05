e7b9c4bd1ac56e493eab698c42ed7876facac29f
Abhay Bhati <abhaybhati@Abhays-MacBook-Pro.local>
2023-04-05 12:46:40 +0530

    new version

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='e2e_client',
-    version='0.1.0',
+    version='1.1.0',
     description="This a E2E Client tool for Certbot Plugin",
     author="Abhay Bhati",
     author_email="abhaybhati987@gmail.com",
     packages=find_packages(),
     install_requires=['requests', 'setuptools'],
-
+    package_data={
+        '': ['*.1'],
+        '': ['docs/*.1'],
+        'docs': ['*.1'],
+    },
+ 
     include_package_data = True,
 )
