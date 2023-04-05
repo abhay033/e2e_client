a940b29e3c2b54b98c0bb6790a1a07d4b1b2aef7
Abhay Bhati <abhaybhati@192.168.1.45>
2023-04-05 23:51:54 +0530

    new version

--- e2e_client/manager.py
+++ e2e_client/manager.py
@@ -7,12 +7,15 @@ class Manager:
         self.api_token = kwargs['api_token']
 
     def check_token(self):
-        my_payload = {}
-        API_key=self.api_key
-        Auth_Token=self.api_token
-        url = BASE_URL+"myaccount/api/v1/e2e_dns/forward/?apikey="+API_key+"&contact_person_id=null&location=Delhi"
-        req="GET"
-        status=Request(url, Auth_Token, my_payload, req).response.json()
-        if status['responseCode']:
-            raise Exception("Token or key is invalid")
-        return True
+        try:
+            my_payload = {}
+            API_key=self.api_key
+            Auth_Token=self.api_token
+            url = BASE_URL+"myaccount/api/v1/e2e_dns/forward/?apikey="+API_key+"&contact_person_id=null&location=Delhi"
+            req="GET"
+            status=Request(url, Auth_Token, my_payload, req).response.json()
+            return True
+        except:    
+            if status['responseCode']:
+                raise Exception("Token or key is invalid")
+            
